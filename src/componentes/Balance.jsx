5cd0b7c7a24c427daf52a4bf5bf77b6963dab0a6
Victor_PB <victorpb@me.com>
2024-02-14 19:31:12 +0100

    balance

--- src/componentes/Balance.jsx
+++ src/componentes/Balance.jsx
@@ -1,5 +1,33 @@
-
+const {ethereum} = window
+import { ethers} from "ethers"
+import { useEffect,useState } from "react"
 
 export function Balance(){
-    return(<h2>Balanc2222e</h2>)
+    const [cuenta, setCuenta] = useState(null)
+    useEffect(()=>{
+        ethereum && ethereum.request({method:'eth_requestAccounts'}).then(cuenta =>{
+            setCuenta(cuenta[0])
+            ethereum.on('accountsChanged', (i) =>{
+                setCuenta(i[0])
+            })
+        })
+    },[])
+    useEffect(()=>{
+        if(cuenta)    {
+            const provider = new ethers.providers.Web3Provider(ethereum)
+            provider.getBalance(cuenta).then(balance =>{
+                console.log(ethers.utils.fromatEther(balance))
+            })
+        }
+    },[cuenta])
+
+    if (!ethereum){
+        return <div>No hay metamask</div>
+    }
+    return(
+        <h2>{
+            cuenta ? cuenta: 'Cargando...'
+            }
+           </h2>
+    )
 }
