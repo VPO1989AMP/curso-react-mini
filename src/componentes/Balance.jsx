d9e266065a75a708fa414603ee11c8c030aacc33
Victor_PB <victorpb@me.com>
2024-02-19 20:03:49 +0100

    errores

--- src/componentes/Balance.jsx
+++ src/componentes/Balance.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ethers } from "ethers"; // Se añade la importación de ethers aquí
+import { ethers } from "ethers";
 import { useFormAction } from "react-router-dom";
 import{useForm} from 'react-hook-form'
 const {ethereum} = window
@@ -7,9 +7,11 @@ const {ethereum} = window
 
 
 export function Balance() {
+    const {register, handleSubmit} = useForm();
     const [cuenta, setCuenta] = useState(null);
     const [balance, setBalance] = useState(null);
-    const {register, handleSubmit} = useForm();
+    const [ok, setOk]= useState(null);
+    const [ko, setKo]= useState(null);
 
     useEffect(() => {
         if (window.ethereum) {
@@ -24,40 +26,59 @@ export function Balance() {
 
     useEffect(() => {
         if (cuenta) {
-            //const provider = new ethers.providers.Web3Provider(window.ethereum);
             const provider = new ethers.BrowserProvider(window.ethereum)
             provider.getBalance(cuenta).then(bal => {
-                console.log(ethers.utils.formatEther(bal));
-                setBalance(ethers.utils.formatEther(bal));
+                console.log(ethers.formatEther(bal));
+                setBalance(ethers.formatEther(bal));
             }).catch(err => console.error(err));
         }
     }, [cuenta]);
 
-    function submit(data){
-        console.log(data)
+    async function submit(data){
+        setKo(null)
+        setOk(null)
+        const parametros ={
+            from:cuenta,
+            to: data.address,
+            value: ethers.utils.parseEther(data.amount).toHexString()
+        }
+        console.log(parametros)
+        try{
+            const txHash = await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [parametros]
+            })
+            setOk(txHash)
+        } catch (error) {
+            setKo("error en la transacción", error.message)
+
+        }
     }
 
-    if (!ethereum) {
-        return <div>No hay MetaMask</div>;
+        if (!ethereum) {
+            return <div>No hay MetaMask</div>;
+        }
+
+        return (
+            <div>
+                <p>Cuenta:{cuenta ? cuenta : 'Cargando...'}</p>
+                <p>Saldo:{balance ? balance : 'Cargando balance...'}</p>
+                <form className="form-inline" onSubmit={handleSubmit(submit)}>
+                    <div className="form-group mb-3">
+                        <label htmlFor="address">Address</label>
+                        <input defaultValue="0x3b5793536578224Dd7160aa23ABe829eaddb2225" id="address" className="form-control" {...register("addresss")}/>
+
+                    </div>
+                    <div className="form-group mb-3">
+                        <label htmlFor="amount">Amount</label>
+                        <input defaultValue="0.0012" id="amount" className="form-control" {...register("amount")}/>
+
+                    </div>
+                    <button type="submit" className="btn btn-primary mb-3">Send</button>
+                </form>
+                {ok && <div className="alert alert-info mt-3">{ok}</div>}
+                {ko && <div className="alert alert-danger mt-3">{ko}</div>}
+            </div>
+        );
     }
 
-    return (
-        <div>
-            <p>Cuenta:{cuenta ? cuenta : 'Cargando...'}</p>
-            <p>Saldo:{balance ? balance : 'Cargando balance...'}</p>
-            <form className="form-inline" onSubmit={handleSubmit(submit)}>
-                <div className="form-group mb-3">
-                    <label htmlFor="address">Address</label>
-                    <input id="address" className="form-control" {...register("addresss")}/>
-
-                </div>
-                <div className="form-group mb-3">
-                    <label htmlFor="amount">Amount</label>
-                    <input id="amount" className="form-control" {...register("addresss")}/>
-
-                </div>
-                <button type="submit" className="btn btn-primary mb-3">Send</button>
-            </form>
-        </div>
-    );
-}
